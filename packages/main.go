6f4590df219508565d056795803b821b412accb7
Ruben Mateus <ruben.mateus@hexis-hub.com>
2018-10-24 18:16:12 +0100

    more play around with strings

--- packages/main.go
+++ packages/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 
 	"github.com/rubenmateus/go-playground/packages/stringutils"
 )
@@ -12,4 +13,23 @@ func main() {
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(16))
 	fmt.Println(stringutils.Reverse("olleh"))
+	fmt.Println(RepeatStr(5, "ola"))
+	fmt.Println(RepeatStr1(5, "ola"))
+
+	// lol
+	panic("aaaaaaaaaaaaaaa")
+}
+
+// simple repeat could be achieved several ways
+func RepeatStr(repetitions int, value string) string {
+
+	var str string
+	for i := 0; i < repetitions; i++ {
+		str += value
+	}
+	return str
+}
+
+func RepeatStr1(repetitions int, value string) string {
+	return strings.Repeat(value, repetitions)
 }
