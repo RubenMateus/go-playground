29268d283b59fea88237ca5c77955aa4e2fee605
Ruben Mateus <ruben.mateus3@gmail.com>
2018-11-21 23:20:49 +0000

    some more playaround

--- functions/main.go
+++ functions/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func greeting(name string) string {
 	return "Hello " + name
@@ -16,8 +19,56 @@ func split(sum int) (x, y int) {
 	return
 }
 
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
 	fmt.Println(greeting("Ruben"))
 	fmt.Println(getSum(1, 2))
 	fmt.Println(split(17))
+
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+
+	// you might notice that functions with a pointer argument must take a pointer:
+	var v1 Vertex
+	//ScaleFunc(v1, 5)  // Compile error!
+	ScaleFunc(&v1, 5) // OK
+
+	// while methods with pointer receivers take either a value or a pointer as the receiver when they are called:
+	var v2 Vertex
+	v1.Scale(5) // OK
+	p := &v2
+	p.Scale(10) // OK
+
+	// Functions that take a value argument must take a value of that specific type:
+
+	var v3 Vertex
+	fmt.Println(AbsFunc(v3)) // OK
+	//fmt.Println(AbsFunc(&v3)) // Compile error!
+	//while methods with value receivers take either a value or a pointer as the receiver when they are called:
+
+	var v4 Vertex
+	fmt.Println(v.Abs()) // OK
+	p2 := &v4
+	fmt.Println(p2.Abs()) // OK
 }
--- interfaces/main.go
+++ interfaces/main.go
@@ -29,10 +29,73 @@ func GetArea(s Shape) float64 {
 	return s.Area()
 }
 
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
 func main() {
 	circle := Circle{0, 0, 5}
 	rectangle := Rectangle{10, 5}
 
+	//A type implements an interface by implementing its methods. There is no explicit declaration of intent, no //"implements" keyword.
+	//Implicit interfaces decouple the definition of an interface from its implementation, which could then appear //in any package without prearrangement.
+
 	fmt.Printf("Circle Area: %f\n", GetArea(circle))
 	fmt.Printf("Rectangle Area: %f\n", GetArea(rectangle))
+
+	var i I = T{"hello"}
+	i.M()
+
+	//An empty interface may hold values of any type. (Every type implements at least zero methods.)
+	var inter interface{}
+	describe(i)
+
+	inter = 42
+	describe(inter)
+
+	inter = "hello"
+	describe(inter)
+
+	var cena interface{} = "hello"
+
+	s := cena.(string)
+	fmt.Println(s)
+
+	s, ok := cena.(string)
+	fmt.Println(s, ok)
+
+	f, ok := cena.(float64)
+	fmt.Println(f, ok)
+
+	//f = cena.(float64) // panic
+	//fmt.Println(f)
+
+	do(21)
+	do("hello")
+	do(true)
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
 }
