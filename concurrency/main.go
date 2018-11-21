4969ff72678c4c4776d6b3843d0ef6be34f32fe8
Ruben Mateus <ruben.mateus3@gmail.com>
2018-11-21 23:58:04 +0000

    added more examples and stuff from concurrency

---
+++ concurrency/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	go say("world")
+	say("hello")
+
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+
+	fmt.Println(ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+
+	channel := make(chan int, 10)
+	go fibonacci(cap(channel), channel)
+	for i := range channel {
+		fmt.Println(i)
+	}
+
+	//The default case in a select is run if no other case is ready.
+	//Use a default case to try a send or receive without blocking:
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
